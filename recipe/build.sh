905f47fe1efe6e76ffd0fd6fd0fc3d401c489c92
Tobias Fischer <info@tobiasfischer.info>
2024-03-08 14:32:34 +1000

    Fixes for cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-./configure --disable-dependency-tracking --disable-sdltest --prefix=${PREFIX}
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+if [[ $target_platform == "osx-arm64" ]] || [[ $target_platform == "linux-aarch64" ]]; then
+  ./configure --disable-dependency-tracking --disable-sdltest --disable-mmx --prefix=${PREFIX}
+else
+  ./configure --disable-dependency-tracking --disable-sdltest --prefix=${PREFIX}
+fi
 make install
